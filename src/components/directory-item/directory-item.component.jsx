d5448578982cb116bddce3a90c94632f5c145c3a
Ryan <ryanschnarre@gmail.com>
2022-05-10 14:39:38 -0400

    added homepage navigation to DirectoryItem component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,21 @@
-import './directory-item.styles.scss';
+import { Link } from "react-router-dom";
+import "./directory-item.styles.scss";
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className='directory-item-container'>
+    <div className="directory-item-container">
       <div
-        className='background-image'
+        className="background-image"
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className='body'>
+      <Link className="body" to={"/shop/" + title}>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Link>
+    </div> 
   );
 };
 
